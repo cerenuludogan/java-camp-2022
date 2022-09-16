a2f34190f4e5cc7cd0e6b0a6be048fd2631a2dfd
Ceren Uludoğan <62713981+cerenuludogan@users.noreply.github.com>
2022-09-16 13:06:05 +0300

    update

--- week1/homeworks/datatypes/src/datatypes/Main.java
+++ week1/homeworks/datatypes/src/datatypes/Main.java
@@ -1,9 +1,16 @@
 package datatypes;
+//https://www.youtube.com/playlist?list=PLqG356ExoxZUGwbqoJEKSMnaxVJe4Uvf8
+//"Java Dersleri 7"
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double sayi = 12.5;
+		sayi = -120;
+		char karakter = 'A';
+		boolean dogruMu = false;
+	
 
 	}
 
