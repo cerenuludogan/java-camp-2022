87a5ec7dc9cb0695f8366d922fce22cbe909c1dc
Ceren Uludoğan <62713981+cerenuludogan@users.noreply.github.com>
2022-09-16 12:34:27 +0300

    update

---
+++ week1/intro/src/intro/Main.java
@@ -0,0 +1,47 @@
+package intro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		
+		String ortaMetin = "İlginizi Çekebilir";
+		String altMetin = "Vade süresi";
+		
+		System.out.println(ortaMetin);
+		
+		int vade = 10;
+		
+		System.out.println(altMetin+": "+vade);
+		
+		double dolarDun = 18.15;
+		double dolarBugun = 18.04;
+		
+		boolean dolarDustuMu = false;
+		
+		String okYonu;
+		
+		if(dolarBugun < dolarDun) {
+			okYonu = "down.svg";
+		}
+		else if(dolarBugun > dolarDun) {
+			okYonu = "up.svg";
+		}
+		else {
+			okYonu = "equal.svg";
+		}
+		
+		System.out.println(okYonu);
+		
+		String[] krediler = {"Hızlı Kredi","Maaşını Halkbanktan","Mutlu Emekli"};
+		
+		for (int i = 0; i < krediler.length; i++) {
+			System.out.println(krediler[i]);
+		}
+		
+	}
+
+	
+
+}
