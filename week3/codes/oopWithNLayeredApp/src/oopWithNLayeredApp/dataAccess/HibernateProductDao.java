62e0bcfad343739a90d5177a238e0513d530f7cf
Ceren Uludoğan <62713981+cerenuludogan@users.noreply.github.com>
2022-10-08 00:00:04 +0300

    update

--- week3/codes/oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
+++ week3/codes/oopWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -2,13 +2,10 @@ package oopWithNLayeredApp.dataAccess;
 
 import oopWithNLayeredApp.entities.Product;
 
-public class JdbcProductDao  implements ProductDao{
-	
+public class HibernateProductDao implements ProductDao {
+
 	public void add(Product product) {
 		//Sadece ve sadece db erişim kodları buraya yazılır..SQL
-		System.out.println("JDBC ile veritabanına eklendi");
+		System.out.println("Hibernate ile veritabanına eklendi");
 	}
-	
-	
-
 }
